2f806dadbe7837dea150089b2763c57642994c51
Valentin Malissen <keisuke.tk.fd@gmail.com>
2020-07-17 20:19:48 -0400

    Initial Commit

---
+++ UI-FoodApp/UI_FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_FoodAppApp.swift
+//  UI-FoodApp
+//
+//  Created by Valentin Malissen on 2020-07-17.
+//
+
+import SwiftUI
+
+@main
+struct UI_FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
